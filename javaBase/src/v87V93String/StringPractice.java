32fba469b32b740f73211fea71c0489039a39031
lina-22 <haquefahinur@gmail.com>
2023-06-18 18:39:28 +0200

    String methods

---
+++ javaBase/src/v87V93String/StringPractice.java
@@ -0,0 +1,14 @@
+package v87V93String;
+
+public class StringPractice {
+
+    public static void main(String[] args) {
+        char [] li = {'l','i','n','a'};
+        System.out.println(li);
+
+        String[] ln = {"ina","ahi" ,"q"};
+       for (String x : ln){
+           System.out.println(x);
+       }
+    }
+}
